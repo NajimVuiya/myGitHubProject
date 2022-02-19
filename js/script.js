497a840fe2c81c969dcb9d76531915685b01eb9f
Gias uddin vuiya <giasuddin6900@gmail.com>
2022-02-19 23:58:31 +0600

    My updated virsion - 3

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@
 
 const box1Select = document.querySelector('.box-1');
 const box2Select = document.querySelector('.box-2');
-
+const social = document.querySelector('#social');
 
 console.log(box1Select);
 console.log(box2Select);
-
+console.log(social);
