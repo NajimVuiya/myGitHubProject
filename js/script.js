ba6905007b9a74156047c17766ac89295eef942f
Gias uddin vuiya <giasuddin6900@gmail.com>
2022-02-19 23:46:35 +0600

    my updated virsion-2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 'use strict';
 
 const box1Select = document.querySelector('.box-1');
-console.log('Hello world This is social Network');
+const box2Select = document.querySelector('.box-2');
+
+
 console.log(box1Select);
+console.log(box2Select);
+
